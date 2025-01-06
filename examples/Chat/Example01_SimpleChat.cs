8ec0baf2386fe63dd513dda585df80e7e117cf86
Anthony Ramos <misky530@aliyun.com>
2025-01-06 15:34:23 +0800

    一小步,成功通过代理调用Openai

--- examples/Chat/Example01_SimpleChat.cs
+++ examples/Chat/Example01_SimpleChat.cs
@@ -1,6 +1,8 @@
-﻿using NUnit.Framework;
+﻿using System;
+using System.ClientModel;
+using System.Diagnostics;
+using NUnit.Framework;
 using OpenAI.Chat;
-using System;
 
 namespace OpenAI.Examples;
 
@@ -9,10 +11,32 @@ public partial class ChatExamples
     [Test]
     public void Example01_SimpleChat()
     {
-        ChatClient client = new(model: "gpt-4o", apiKey: Environment.GetEnvironmentVariable("OPENAI_API_KEY"));
+        ChatClient client = new(model: "gpt-3.5-turbo", apiKey: Environment.GetEnvironmentVariable("OPENAI_API_KEY"));
 
         ChatCompletion completion = client.CompleteChat("Say 'this is a test.'");
 
         Console.WriteLine($"[ASSISTANT]: {completion.Content[0].Text}");
+    }
+
+    [Test]
+    public void Example02_SimpleChat()
+    {
+        var option = new OpenAIClientOptions
+        {
+            Endpoint = new Uri("https://api.openai-proxy.org/v1")
+        };
+
+        var apiKey = Environment.GetEnvironmentVariable("OPENAI_API_KEY");
+        Debug.Assert(apiKey != null, nameof(apiKey) + " != null");
+        var credentials = new ApiKeyCredential(apiKey);
+
+        // 初始化 OpenAIClient
+        var client = new OpenAIClient(credentials, option);
+        var chat = client.GetChatClient(model: "gpt-3.5-turbo");
+
+        ChatCompletion completion = chat.CompleteChat("Say 'this is a test.'");
+
+        Console.WriteLine($"[ASSISTANT]: {completion.Content[0].Text}");
+
     }
 }
